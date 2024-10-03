b896520e61f92314e9d20939d67c48e8888f037a
bigsam08 <uelsam994@gmail.com>
2024-10-03 16:13:28 +0100

    update

---
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -0,0 +1,44 @@
+class HolbertonCourse {
+  constructor(name, length, students) {
+    this.name = name;
+    this.length = length;
+    this.students = students;
+  }
+
+  set name(name) {
+    if (typeof name !== 'string') {
+      throw new TypeError('Name must be a string');
+    }
+
+    this._name = name;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  set length(length) {
+    if (typeof length !== 'number') {
+      throw new TypeError("Length must be a number");
+    }
+
+    this._length = length;
+  }
+
+  get length() {
+    return this._length;
+  }
+
+  set students(students) {
+    if (!Array.isArray(students)) {
+      throw new TypeError("Students must be an array");
+    }
+    this._students = students;
+  }
+
+  get student() {
+    return this._students;
+  }
+}
+
+export default HolbertonCourse;
