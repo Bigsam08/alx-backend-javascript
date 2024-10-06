27a18d7c3a18120243bf18ad3bb37e8a4facd9ef
bigsam08 <uelsam994@gmail.com>
2024-10-06 04:26:39 +0100

    advance task

---
+++ 0x01-ES6_promise/100-await.js
@@ -0,0 +1,14 @@
+import { uploadPhoto, createUser } from './utils';
+
+export default async function asyncUploadUser() {
+  let dict = {};
+
+  try {
+    const photo = await uploadPhoto();
+    const user = await createUser();
+    dict = { photo, user };
+  } catch (err) {
+    dict = { photo: null, user: null };
+  }
+  return dict;
+}
